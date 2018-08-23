678872db0c583ea7bcec8cd4f953839cd5ac7197
Claudio Rojas <claudio@MacBook-Pro-de-Claudio.local>
2018-08-23 00:01:09 -0300

    added multiple features YEAH

--- src/index.js
+++ src/index.js
@@ -7,14 +7,79 @@ import Table from "./table/Table.jsx";
 import Col from "./table/Col.jsx";
 import data from "./data.json";
 
-function App() {
-  return (
-    <div className="App">
-      <Table data={data}>
-        <Col accessor="id" />
-      </Table>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: data,
+    };
+
+    this.handlerOnClick = this.handlerOnClick.bind(this);
+    this.handlerOnRemove = this.handlerOnRemove.bind(this);
+  }
+
+  handlerOnClick() {
+    this.setState((prevState) => {
+      const l = prevState.data;
+      l.push({
+        "id": 1,
+        "name": "Claudio",
+        "extra": {
+          "country": "Chile"
+        }
+      });
+      return {
+        ...prevState,
+        data: l,
+      }
+    });
+  }
+
+  handlerOnRemove(element, index, data) {
+    this.setState((prevState) => {
+      const ind = index;
+      const d = data;
+
+      const l = prevState.data.filter((e, i) => {
+        return i !== index;
+      });
+      return {
+        ...prevState,
+        data: l,
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.handlerOnClick}>Add</button>
+        <Table data={this.state.data}>
+          <Col
+            isNumeric
+            accessor="id"
+            className="color-danger"
+          />
+          <Col
+            accessor="name"
+          />
+          <Col
+            title="Acción"
+            className="color-danger"
+            template={(e, i, d) => <button onClick={() => {
+              this.handlerOnRemove(e, i ,d);
+            }}>{d.id} [>] </button>}
+          />
+          <Col
+            defaultSort
+            accessor="extra.country"
+            template={(e, i, d) => (<strong>-{i}-</strong>)}
+          />
+        </Table>
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
--- src/table/Table.jsx
+++ src/table/Table.jsx
@@ -1,41 +1,149 @@
 import React, { Component } from "react";
-import { compareMake } from "./TableUtils";
+import { compareMake, dotNot } from "./TableUtils";
+
+const CLASS_NAME = {
+	TABLE: 'ui-kit-table',
+	ACTIVE: 'ui-kit-table__th-active',
+	SORT_DOWN: 'ui-kit-table__th-sort-up',
+	SORT_UP: 'ui-kit-table__th-sort-down',
+};
 
 class Table extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: props.data,
-      sortBy: "value",
-      reverse: false,
-      isNumeric: false
-    };
-    this.onSort = this.onSort.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: props.data,
+			sortBy: '',
+			reverse: false,
+			isNumeric: false,
+			configCol: [],
+			self: this,
+		};
+		this.onSort = this.onSort.bind(this);
+	}
 
-  onSort(key, isNumeric) {
-    const { sortBy, reverse } = this.state;
-    this.setState({
-      sortBy: key,
-      reverse: sortBy === key ? !reverse : false,
-      isNumeric: isNumeric !== undefined
-    });
-  }
+	checkType(e, type) {
+		const ErrorType = function (el, ty) {
+			this.toString = () => `the element must be of type '${ty}' and not '${el}'`;
+		}
+		const elementType = e.type.name || e.type;
+		console.log(elementType === type);
+		if (elementType === type) {
+			return true;
+		} else {
+			throw new ErrorType(elementType, type);
+		}
+	}
 
-  render() {
-    const { data, sortBy, reverse, isNumeric } = this.state;
-    return (
-      <div>
-        {JSON.stringify(reverse)} -
-        {JSON.stringify(isNumeric)}
-        <button onClick={() => this.onSort("id", true)}>id</button>
-        <button onClick={() => this.onSort("value")}>name</button>
-        <button onClick={() => this.onSort("extra.country")}>country</button>
-        {JSON.stringify(data.sort(compareMake(sortBy, reverse, isNumeric)))}
-        {data.map(e => <strong>{e.id}</strong>)}
-      </div>
-    );
-  }
+	componentDidMount() {
+		this.config();
+	}
+
+	static getDerivedStateFromProps(p, o) {
+		const dat = {
+			...o,
+			data: p.data,
+		};
+		return dat;
+	}
+
+	config() {
+		const newConfigCol = this.state.configCol;
+		let sortBy = '';
+		React.Children.map(this.props.children, (e, i) => {
+			this.checkType(e, 'Col');
+			// selected initial sort by
+			if (e.props.defaultSort) {
+				sortBy = e.props.accessor;
+			}
+
+			newConfigCol.push({
+				accessor: e.props.accessor,
+				colProps: {
+					className: e.props.className,
+				},
+				template: e.props.template,
+				isNumeric: e.props.isNumeric,
+				headerProps: {
+					title: e.props.title || e.props.accessor || '',
+				},
+			});
+		});
+
+		// if sort by dont exist, assign first accessor
+		if (!sortBy) {
+			sortBy = newConfigCol[0].accessor;
+		}
+
+		this.setState({
+			sortBy,
+			configCol: newConfigCol,
+		});
+	}
+
+	onSort(key, isNumeric) {
+		const { sortBy, reverse } = this.state;
+		this.setState({
+			sortBy: key,
+			reverse: sortBy === key ? !reverse : false,
+			isNumeric: isNumeric !== undefined
+		});
+	}
+
+	makeRow(element, index) {
+		const { configCol } = this.state;
+		return configCol.map((conf, i) => (
+			<td key={i} {...conf.colProps} >
+				{conf.accessor ? (
+					conf.template
+					? conf.template(dotNot(element, conf.accessor), index, element)
+					: dotNot(element, conf.accessor)
+				):(
+					conf.template(null, index, element)
+				)}
+			</td>
+		));
+	}
+
+	render() {
+		const { data, sortBy, reverse, configCol } = this.state;
+		return sortBy && configCol.length > 0 ? (
+			<div>
+				<table className={CLASS_NAME.TABLE}>
+					<thead>
+						<tr>
+						{configCol.map((e, i) => (
+							<th
+								key={i}
+								className={
+									e.accessor === sortBy ? CLASS_NAME.ACTIVE + ' ' + (reverse ? CLASS_NAME.SORT_UP : CLASS_NAME.SORT_DOWN): ''}
+							>
+								{e.accessor ? (<button
+									onClick={() => this.onSort(e.accessor, e.isNumeric)}
+								>
+									{e.headerProps.title}
+									{e.accessor === sortBy && (reverse ? '<' : '>')}
+								</button>
+								) : e.headerProps.title}
+							</th>
+						))}
+						</tr>
+					</thead>
+					<tbody>
+					{(data.sort(compareMake(
+							sortBy,
+							reverse,
+							configCol.find((e) => e.accessor === sortBy).isNumeric
+						))).map((e, i) => (
+						<tr key={i}>
+							{this.makeRow(e, i)}
+						</tr>
+					))}
+					</tbody>
+				</table>
+			</div>
+		) : <div />;
+	}
 }
 
 export default Table;
